1a98264199654a1ffc7cd81d80c20e241c385f9f
Deia Daimon <deiadaimon@gmail.com>
2026-02-05 15:37:25 +0100

    Refactor JavaScript: use addEventListener()

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@
 // ========= MOBILE MENU =========
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuButton = document.querySelector(".mobile-menu-btn");
-mobileMenuButton.onclick = function () {
+mobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.add("is-open");
-};
+});
 
 const closeMenuButton = document.querySelector(".close-menu-btn");
-closeMenuButton.onclick = function () {
+closeMenuButton.addEventListener("click", () => {
   mobileMenu.classList.remove("is-open");
-};
+});
